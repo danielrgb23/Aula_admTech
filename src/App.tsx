6b93e0d11d1ad09d6eafa82f0b5b4b0561c67639
Daniel <danielrgb2@gmail.com>
2022-10-19 15:14:44 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { SalesCard } from './components/SalesCard'
 
 export function App() {
   return (
     <div className="App">
       <Header/>
+      <main>
+        <section id ="sales">
+          <div className='dsmeta-container'>
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
